41961f46760eb71654ccfd422f996de38155911e
Se-Ho Kim <bshk93@gmail.com>
2026-02-05 19:49:41 +0000

    pw remove

--- app/server_modules/_init.R
+++ app/server_modules/_init.R
@@ -1,7 +1,7 @@
 # Server Initialization ----
 # Shared reactives, observers, and helper functions used across modules
 
-myPassword <- "krisdunn"
+myPassword <- ""
 
 # Helper function for popups ----
 popup <- function(input_info, type) {
