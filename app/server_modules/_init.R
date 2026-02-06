86b8d357c5d273c1915e37b19cf062db8eacda60
Se-Ho Kim <bshk93@gmail.com>
2026-02-06 17:18:11 +0000

    pw update

--- app/server_modules/_init.R
+++ app/server_modules/_init.R
@@ -1,7 +1,7 @@
 # Server Initialization ----
 # Shared reactives, observers, and helper functions used across modules
 
-myPassword <- ""
+myPassword <- "gortat"
 
 # Helper function for popups ----
 popup <- function(input_info, type) {
