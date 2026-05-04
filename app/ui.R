c67846410f128677471ab77b8c295fcdea26e818
Se-Ho Kim <bshk93@gmail.com>
2026-05-04 18:35:16 +0000

    Deploy code changes

--- app/ui.R
+++ app/ui.R
@@ -153,15 +153,6 @@ body <- dashboardBody(
       to { opacity: 1; }
     }
 
-    #champion-logos {
-      position: absolute !important;
-      left: 260px;
-      top: 0;
-      height: 50px;
-      display: flex !important;
-      align-items: center;
-      padding: 0 10px;
-    }
   ")),
   
   tags$script(HTML("
@@ -601,8 +592,8 @@ dashboardPage(
       ))),
     titleWidth = 250,
     tags$li(
-      id = "champion-logos",
       class = "dropdown",
+      style = "display:flex;align-items:center;padding:0 10px;",
       HTML(champion_logos_html)
     )
   ),
